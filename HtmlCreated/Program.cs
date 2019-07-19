f37b4e10654e6177878812220e15219436332ff7
Luan Alves <luan.alves@comunike.me>
2019-07-19 10:07:35 -0300

    First Commit

---
+++ HtmlCreated/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlCreated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\comunikime\arquivo.html"))
+            {
+                sw.WriteLine("<HTML>");
+                sw.WriteLine("<head>" +
+                    "<script>" +
+                    "function pode(){alert('Testando o testador')}" +
+                    "</script>" +
+                    "</head>");
+                sw.WriteLine("<TITLE>Hello World...</TITLE>");
+                sw.WriteLine("<BODY>");
+                sw.WriteLine("<H1>Hello World !</H1>");
+                sw.WriteLine("<button onclick='pode()'>Adicionar</button>");
+                sw.WriteLine("</BODY>");
+                sw.WriteLine("</HTML>");
+
+                sw.Close();
+            }
+
+        }
+    }
+}
