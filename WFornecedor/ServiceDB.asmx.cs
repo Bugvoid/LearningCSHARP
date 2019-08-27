72ea475b7da76e976c77917eb1872274989fd6eb
Luan Alves <luan.alves@comunike.me>
2019-08-27 15:47:32 -0300

    Created new Webservices

---
+++ WFornecedor/ServiceDB.asmx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WFornecedor
+{
+    /// <summary>
+    /// Descrição resumida de ServiceDB
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceDB : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+
+        [WebMethod]
+        public DataTable GetDados()
+        {
+            string contr = ConfigurationManager.ConnectionStrings["conexaoProdutos"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(contr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Select * from Produtos"))
+                {
+                    using(SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using(DataTable dt = new DataTable())
+                        {
+                            dt.TableName = "Produtos";
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ W_liteKoll/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using W_liteKoll.br.com.lite.dotnet.service;
+
+namespace W_liteKoll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WCreatedServiceService wService = new WCreatedServiceService();
+            string voltou = wService.ping("Luan Castro Alves");
+            MessageBox.Show("Voltou " + voltou);
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            WCreatedServiceService wService = new WCreatedServiceService();
+            carro[] carros = wService.getMeusCarro();
+            foreach( carro  carroatual in carros)
+            {
+                MessageBox.Show("Voltou um " + carroatual.nome + " da cor " + carroatual.cor);
+            }
+
+        }
+    }
+}
